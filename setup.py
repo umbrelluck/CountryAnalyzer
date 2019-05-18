ae7dfe0890ae42ed9e7921d2561392ac94a80ab5
umbrelluck <umbrelluck@gmail.com>
2019-05-18 18:38:20 +0300

    intro and distro

--- setup.py
+++ setup.py
@@ -1,4 +1,15 @@
-from distutils.core import setup
+from setuptools import find_packages, setup
 
-setup(name="CountryAnalyzer", version="1.0",
-      py_modules=['analyzer', 'country', 'gdeltAPI', 'helper'], scripts=['main.py'], )
+setup(name="CountryAnalyzer",
+      version="1.0",
+      author='Yaroslav Borys',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=['copy',
+                        'datetime',
+                        'json',
+                        'sys',
+                        'urllib.request',
+                        'webbrowser'],
+      )
---
+++ with_GUI/setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(name="CountryAnalyzer",
+      version="1.0",
+      author='Yaroslav Borys',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      )
